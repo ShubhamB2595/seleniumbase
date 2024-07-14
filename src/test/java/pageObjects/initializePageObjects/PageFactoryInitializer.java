4828294a39a42d21b939d00248be8341111279e8
Priyanka7001 <priyanka1.sawarkar@gmail.com>
2024-07-14 18:29:39 +0530

    refactored

--- src/test/java/pageObjects/initializePageObjects/PageFactoryInitializer.java
+++ src/test/java/pageObjects/initializePageObjects/PageFactoryInitializer.java
@@ -5,11 +5,6 @@ import org.openqa.selenium.support.PageFactory;
 import controllers.BaseMethod;
 import pageObjects.modules.HomePage;
 
-/**
- * @author ${Gladson Antony}
- * @date Sep 17, 2016
- */
-
 public class PageFactoryInitializer extends BaseMethod {
 	
 	// create object of class as method and initialize page factory
