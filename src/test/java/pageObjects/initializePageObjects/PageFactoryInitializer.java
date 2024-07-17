8b13cea1ab64475acc6300879df335c672fcfa81
Rutuja <rutujacher97@gmail.com>
2024-07-17 20:51:46 +0530

    Test case added

--- src/test/java/pageObjects/initializePageObjects/PageFactoryInitializer.java
+++ src/test/java/pageObjects/initializePageObjects/PageFactoryInitializer.java
@@ -4,11 +4,17 @@ import org.openqa.selenium.support.PageFactory;
 
 import controllers.BaseMethod;
 import pageObjects.modules.HomePage;
+import pageObjects.modules.LoginPage;
 
 public class PageFactoryInitializer extends BaseMethod {
-	
+
 	// create object of class as method and initialize page factory
 	public HomePage homePage() {
 		return PageFactory.initElements(getWebDriver(), HomePage.class);
 	}
+
+	public LoginPage loginPage() {
+		return PageFactory.initElements(getWebDriver(), LoginPage.class);
+	}
+
 }
