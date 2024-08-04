429c5f98660208cc85a08fc3fa320b23e83d4867
Rutuja <rutujacher97@gmail.com>
2024-08-04 17:08:48 +0530

    Test case add

--- src/test/java/pageObjects/modules/LoginPage.java
+++ src/test/java/pageObjects/modules/LoginPage.java
@@ -8,7 +8,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends PageFactoryInitializer {
-
+	
+	
 	@FindBy(id = "ctl00_lblUser")
 	private WebElement myAccount_Btn;
 
@@ -23,9 +24,27 @@ public class LoginPage extends PageFactoryInitializer {
 
 	@FindBy(id = "ctl00_phBody_SignIn_btnLogin")
 	private WebElement loginIn_Btn;
+	
+	@FindBy(id = "ctl00_phBody_SignIn_lblemailmsg")
+	private WebElement findErrorMsg_Txt;
+	
+	@FindBy(id="ctl00_phBody_SignIn_lblemailmsg")
+	private WebElement accountEmailErrorMsg_Txt;
+	
+	@FindBy(xpath="//*[@id=\"ctl00_phBody_SignIn_lblemailmsg\"]")
+	private WebElement validEmailErrorMsg_Txt;
+	
+	@FindBy(id="ctl00_phBody_SignIn_rfvPassword")
+	private WebElement validPassErrorMsg_Txt;
+	
+	@FindBy(id="ctl00_phBody_SignIn_rfvPassword")
+	private WebElement enterPasswordErrorMsg_Txt;
 
 	@FindBy(id = "ctl00_phBody_SignIn_btnRequestOTP")
 	private WebElement requestOTP_Btn;
+	
+	@FindBy(xpath = "//a[@class='forgotpasswordlink themecolor']")
+	private WebElement forgotyourPassword_Txt;
 
 	public void mouseOver_MyAccount_Btn() {
 		mouseHover(myAccount_Btn);
@@ -74,6 +93,62 @@ public class LoginPage extends PageFactoryInitializer {
 		sendkeys_Password_Txt(password);
 		click_LoginIn_Btn();
 	}
+	
+	public void goto_AccountLogin() {
+		mouseHover(myAccount_Btn);
+		login_Link.click();
+	}
+	
+	public boolean isDisplay_validEmailErrorMsg_Txt() {
+		return validEmailErrorMsg_Txt.isDisplayed();
+	}
+	
+	public boolean isDisplay_AccountEmailErrorMsg_Txt() {
+		return accountEmailErrorMsg_Txt.isDisplayed();
+	}
+	
+	public String getText_AccountEmailErrorMsg_Txt() {
+		return accountEmailErrorMsg_Txt.getText();
+	}
+	
+	
+	public String  getText_ValidemailError() {
+		return validEmailErrorMsg_Txt.getText();
+	}
+	
+	public String getText_FindEmailErrorMsg_Txt() {
+		return findErrorMsg_Txt.getText();
+	}
+	
+	public String getText_EnterPasswordError_Txt(){
+		return enterPasswordErrorMsg_Txt.getText();
+	}
+	
+	
+	public void isDisplay_EmailPasswordLogin_Txt() {
+		emailId_Txt.isDisplayed();
+		 password_Txt.isDisplayed();
+		 loginIn_Btn.isDisplayed();
+	}
+	
+	public void sendkeys_EmailPasswordLogin_Click(String username,String password) {
+		sendKeys_EmailId_Txt(username);
+		sendkeys_Password_Txt(password);
+		click_LoginIn_Btn();
+	}
+	
+	public boolean isDisplay_ForgotyourPassword_Txt() {
+		return  forgotyourPassword_Txt.isDisplayed();
+	}
+	
+	public void click_forgotyourPassword_Txt() {
+		forgotyourPassword_Txt.click();
+		ExplicitWaiting.waitForSomeTime(5000);
+	}
+	
+	
+	
+	
 	
 	
 
